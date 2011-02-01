199c0dd64aacb4c51890a898732c64c90ae1979f
Josh Sullivan <jaysche.sullivan@gmail.com>
2011-02-01 08:57:22 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Josh
+ * Date: Jan 9, 2011
+ * Time: 9:35:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    
+}
